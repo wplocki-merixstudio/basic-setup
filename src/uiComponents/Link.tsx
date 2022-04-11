36c67a3ad3149468a9318dde37bad535a3fccbef
wilq1994 <bartek94@gmail.com>
2022-04-11 10:16:46 +0200

    ui fixes

--- src/uiComponents/Link.tsx
+++ src/uiComponents/Link.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Link = styled.a`
+  color: #1930E5;
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
--- src/uiComponents/Text.tsx
+++ src/uiComponents/Text.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Text = styled.span<{ color?: string }>`
   color: ${(p) => (p.color ? p.color : "var(--typography-primary)")};
-  font-family: "Euclid Circular A, Arial";
+  font-family: "Euclid Circular A", Arial;
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
